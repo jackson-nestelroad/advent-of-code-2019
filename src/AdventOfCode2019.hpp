3f0cc6bf9c60490b1429b157f4118f14331156dd
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-12-08 20:55:59 -0600

    Header file for declarations of all solutions.

---
+++ src/AdventOfCode2019.hpp
@@ -0,0 +1,24 @@
+#ifndef ADVENTOFCODE2019_HPP
+#define ADVENTOFCODE2019_HPP
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <algorithm>
+#include <numeric>
+
+#include "common/Console.hpp"
+#include "common/InputReader.hpp"
+
+namespace aoc {
+    namespace A {
+        int day01();
+    }
+
+    namespace B {
+        int day01();
+    }
+}
+
+#endif
