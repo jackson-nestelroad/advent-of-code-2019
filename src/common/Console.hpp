24b94508e94d4339fbbe90f4a249dd9f3b930e0e
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-12-08 20:55:43 -0600

    Utility functions for printing output.

---
+++ src/common/Console.hpp
@@ -0,0 +1,37 @@
+#ifndef CONSOLE_HPP
+#define CONSOLE_HPP
+
+#include <iostream>
+
+namespace console {
+
+	static std::ostream &console = std::cout;
+
+	inline void log(void) {
+		std::cout << std::endl;
+	}
+
+	template<class First>
+	void log(const First &first) {
+		console << first << std::endl;
+	}
+
+	template<class First, class... Rest>
+	void log(const First &first, const Rest &... rest) {
+		console << first << std::endl;
+		log(rest...);
+	}
+
+	template<class First>
+	void string(const First &first) {
+		console << first << std::endl;
+	}
+
+	template<class First, class... Rest>
+	void string(const First &first, const Rest &... rest) {
+		console << first;
+		string(rest...);
+	}
+}
+
+#endif
