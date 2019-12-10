e788fa9360a5142bd18ac1b742680fc0e975f504
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-12-09 19:07:12 -0600

    Day 2-A and 2-B

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
+#include <string>
 #include <chrono>
+#include <functional>
 
 #include "AdventOfCode2019.hpp"
 
-int main() {
+void run(const std::string &text, const std::function<int()> &solution) {
     auto start = std::chrono::system_clock::now().time_since_epoch();
-
-    console::string("Solution: ", aoc::B::day01());
-    
+    console::log(text, solution());
     auto end = std::chrono::system_clock::now().time_since_epoch();
 
     auto elapsedTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
-    console::string("Time: ", elapsedTime.count(), " ms");
+    console::log("Time: ", elapsedTime.count(), " ms");
+}
 
+int main() {
+    run("Part A: ", AoC::A::day02);
+    run("Part B: ", AoC::B::day02);
     return 0;
 }
---
+++ src/days/02.cpp
@@ -0,0 +1,67 @@
+#include "../AdventOfCode2019.hpp"
+
+namespace IntCode {
+    using Integers = std::vector<int>;
+
+    enum Instructions {
+        Add = 1,
+        Multiply = 2,
+        Exit = 99
+    };
+
+    class Computer {
+    private:
+        Integers data;
+        bool running = false;
+
+    public:
+        Computer(const Integers &data) : data(data) { }
+
+        void run() {
+            int ip = 0;
+            running = true;
+            while (running && ip < data.size()) {
+                switch (data[ip]) {
+                    case Add: data[data[ip + 3]] = data[data[ip + 1]] + data[data[ip + 2]]; ip += 4; break;
+                    case Multiply: data[data[ip + 3]] = data[data[ip + 1]] * data[data[ip + 2]]; ip += 4; break;
+                    case Exit: running = false; break;
+                    default: console::fatal("Invalid opcode at position ", ip, ".");
+                }
+            }
+            running = false;
+        }
+
+        int &operator[](int i) {
+            return data[i];
+        }
+    };
+}
+
+int AoC::A::day02() {
+    auto input = InputReader::readFile<int, ','>("input/02.txt");
+    IntCode::Computer comp { input };
+
+    comp[1] = 12;
+    comp[2] = 2;
+
+    comp.run();
+
+    return comp[0];
+}
+
+int AoC::B::day02() {
+    auto input = InputReader::readFile<int, ','>("input/02.txt");
+
+    for (int noun = 0; noun <= 99; ++noun) {
+        input[1] = noun;
+        for (int verb = 0; verb <= 99; ++verb) {
+            input[2] = verb;
+            IntCode::Computer comp { std::vector(input) };
+            comp.run();
+            if (comp[0] == 19690720) {
+                return 100 * noun + verb;
+            }
+        }
+    }
+    return -1;
+}
