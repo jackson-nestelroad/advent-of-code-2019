31c9ba417208544d3ac70d1f3d92d7c93e6877fd
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-12-09 19:06:56 -0600

    Move project organization around.

---
+++ src/days/01.cpp
@@ -0,0 +1,29 @@
+#include "../AdventOfCode2019.hpp"
+
+inline int fuelRequirement(int mass) {
+    return (mass / 3) - 2;
+}
+
+int fuelRequirementRecursive(int mass, int result = 0) {
+    int fuel = fuelRequirement(mass);
+    if (fuel <= 0) {
+        return result;
+    }
+    return fuelRequirementRecursive(fuel, result + fuel);
+}
+
+int AoC::A::day01() {
+    auto input = InputReader::readFile<int>("input/01.txt");
+
+    std::transform(input.begin(), input.end(), input.begin(), fuelRequirement);
+
+    return std::accumulate(input.begin(), input.end(), 0, std::plus<int>());
+}
+
+int AoC::B::day01() {
+    auto input = InputReader::readFile<int>("input/01.txt");
+
+    std::transform(input.begin(), input.end(), input.begin(), [](auto mass) { return fuelRequirementRecursive(mass); });
+
+    return std::accumulate(input.begin(), input.end(), 0, std::plus<int>());
+}
