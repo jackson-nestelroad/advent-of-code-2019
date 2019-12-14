e9c1c7ae0d7e83f878ac1ced907d437986815ab8
jackson-nestelroad <40396320+jackson-nestelroad@users.noreply.github.com>
2019-12-13 22:47:55 -0600

    Non-const logging functions to allow for mutable output operations.

--- src/common/Console.hpp
+++ src/common/Console.hpp
@@ -20,6 +20,17 @@ namespace console {
         log(rest...);
     }
 
+    template <class First>
+    void log(First &first) {
+        console << first << std::endl;
+    }
+
+    template <class First, class... Rest>
+    void log(First &first, Rest &... rest) {
+        console << first << ' ';
+        log(rest...);
+    }
+
     inline void fatal(void) {
         console << std::endl;
         exit(-1);
@@ -33,6 +44,18 @@ namespace console {
 
     template <class First, class... Rest>
     void fatal(const First &first, const Rest &... rest) {
+        console << first;
+        fatal(rest...);
+    }
+
+    template <class First>
+    void fatal(First &first) {
+        console << first << std::endl;
+        exit(-1);
+    }
+
+    template <class First, class... Rest>
+    void fatal(First &first, Rest &... rest) {
         console << first;
         fatal(rest...);
     }
